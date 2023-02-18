19c8c519295ede3e53002c62f8ba7ec9d2d94106
Rafael Almeida <rafaelpbcp@gmail.com>
2023-02-18 15:06:45 -0500

    first commit

---
+++ fbla2023_server/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fbla2023_server project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fbla2023_server.settings')
+
+application = get_wsgi_application()
