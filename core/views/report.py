6e8a421dac3767c465a8eca1c4938af59b0a0e36
Rafael Almeida <rafaelpbcp@gmail.com>
2023-03-05 17:00:19 -0500

    reports now award prizes

--- core/views/report.py
+++ core/views/report.py
@@ -3,7 +3,7 @@ from datetime import date
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from ..models import Report, Student, Attendance, Leaderboard
+from ..models import Report, Student, Attendance, Leaderboard, Prize
 from ..serializers.report import ReportSerializer, ReportListSerializer
 
 
@@ -11,9 +11,13 @@ from ..serializers.report import ReportSerializer, ReportListSerializer
 def report_create_view(request):
     """Creates a Report, resets students' points and rank, and marks all attendances as final."""
 
-    students = Student.objects.all()
+    students = Student.objects.all().order_by('-live_points')
     today = date.today()
-    for student in students:
+    for i, student in enumerate(students):
+        if i in [0, 1,  2]:
+            prizes = ["Spirit", "School", "Food"]
+            Prize(student=student, type=prizes[i]).save()
+
         report = Report(created_on=today, first_name=student.first_name or student.email, middle_name=student.middle_name,
                         last_name=student.last_name, points=student.live_points, grade=student.grade)
         report.save()
@@ -49,6 +53,6 @@ def report_retrieve_view(request, created_on):
     """Lists all Report objects given a date."""
 
     reports = Report.objects.all().filter(
-        created_on=created_on)
+        created_on=created_on).order_by("last_name", "first_name", "middle_name")
     serializer = ReportSerializer(reports, many=True)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
