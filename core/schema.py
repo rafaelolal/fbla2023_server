246cde6b120b28fffda74133e128cac298b37081
Rafael Almeida <rafaelpbcp@gmail.com>
2023-06-27 21:10:48 -0400

    added prize redemption to schema

--- core/schema.py
+++ core/schema.py
@@ -3,9 +3,11 @@ from rest_framework import serializers
 import graphene
 from graphene_django import DjangoObjectType
 from graphene_django.rest_framework.mutation import SerializerMutation
-from .models import Admin, AdminAnnouncement, Group, GroupMember, GroupEvent, GroupAnnouncement, Student, Event, EventFeedback, Attendance, News, Report, Rally, Leaderboard, Prize
+from .models import PrizeRedemption, Admin, AdminAnnouncement, Group, GroupMember, GroupEvent, GroupAnnouncement, Student, Event, EventFeedback, Attendance, News, Report, Rally, Leaderboard, Prize
 from .serializers.admin import AdminAnnouncementSerializer
 from .serializers.event_feedback import EventFeedbackSerializer
+from datetime import datetime
+from django.utils import timezone
 
 
 class StudentType(DjangoObjectType):
@@ -53,16 +55,26 @@ class LeaderboardType(DjangoObjectType):
         field = ('id', 'created_on')
 
 
-class PrizeType(DjangoObjectType):
-    class Meta:
-        model = Prize
-        field = ('id', "type", 'student')
-
-
 class AdminType(DjangoObjectType):
     class Meta:
         model = Admin
         field = ("id")
+
+################################
+
+
+class PrizeType(DjangoObjectType):
+    class Meta:
+        model = Prize
+        field = ('id', 'name', 'type', 'cost')
+
+
+class PrizeRedemptionType(DjangoObjectType):
+    class Meta:
+        model = PrizeRedemption
+        field = ('id', 'prize', 'student', 'redeemed_on', 'is_approved')
+
+##############################
 
 
 class GroupMemberType(DjangoObjectType):
@@ -129,6 +141,7 @@ class Query(graphene.ObjectType):
         EventType, type=graphene.String(), count=graphene.Int())
     retrieve_group = graphene.Field(GroupType, id=graphene.String())
     list_groups = graphene.List(GroupType)
+    list_prize_redemptions = graphene.List(PrizeRedemptionType)
 
     def resolve_retrieve_admin(root, info, id):
         return Admin.objects.get(id=id)
@@ -137,14 +150,16 @@ class Query(graphene.ObjectType):
         return AdminAnnouncement.objects.all()
 
     def resolve_list_events_by_type(root, info, type, count):
-        return Event.objects.all().filter(
-            type=type).order_by('-starts_on')[:count or None]
+        return Event.objects.all().filter(starts_on__gte=datetime.now(tz=timezone.utc)).filter(type=type).order_by('-starts_on')[:count or None]
 
     def resolve_retrieve_group(root, info, id):
         return Group.objects.get(id=id)
 
     def resolve_list_groups(root, info):
         return Group.objects.all().filter(is_private=False)
+
+    def resolve_list_prize_redemptions(root, info):
+        return PrizeRedemption.objects.all().filter(is_approved=False)
 
 
 class Mutation(graphene.ObjectType):
